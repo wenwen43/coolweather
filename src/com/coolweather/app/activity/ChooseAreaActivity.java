d562bf3648de9bfe0bad687fd0d74769c5ffd1db
wenwen43 <583594780@qq.com>
2015-11-09 19:52:19 +0800

    加入显示天气信息的功能（但有bug）。”

--- src/com/coolweather/app/activity/ChooseAreaActivity.java
+++ src/com/coolweather/app/activity/ChooseAreaActivity.java
@@ -14,7 +14,10 @@ import com.coolweather.app.util.Utility;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -65,12 +68,19 @@ public class ChooseAreaActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if (prefs.getBoolean("city_selected", false)) {
+			Intent intent = new Intent(this, WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
 		requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题栏 
 		setContentView(R.layout.choose_area);
 		listView = (ListView) findViewById(R.id.list_view);
 		titleText = (TextView) findViewById(R.id.title_text);
-		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dataList);//初始胡ArrayAdapter，将其设置为ListView的适配器
-		listView.setAdapter(adapter);////将ArrayAdapter的值映射到ListView中
+		adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,dataList);//初始化ArrayAdapter，将其设置为ListView的适配器
+		listView.setAdapter(adapter);//将ArrayAdapter的值映射到ListView中
 		coolWeatherDB = CoolWeatherDB.getInstance(this);//获取CoolWeatherDB的实例
 		
 		listView.setOnItemClickListener(new OnItemClickListener() {
@@ -82,7 +92,13 @@ public class ChooseAreaActivity extends Activity {
 					} else if (currentLevel == LEVEL_CITY) {
 						selectedCity = cityList.get(index);
 						queryCounties();
-						}
+					} else if (currentLevel == LEVEL_COUNTY) {
+						String countyCode = countyList.get(index).getCountyCode();
+						Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+						intent.putExtra("county_code", countyCode);
+						startActivity(intent);
+						finish();
+					}
 				}
 			});
 		queryProvinces(); // 加载省级数据
@@ -92,8 +108,8 @@ public class ChooseAreaActivity extends Activity {
 	* 查询全国所有的省，优先从数据库查询，如果没有查询到再去服务器上查询。
 	*/
 	private void queryProvinces() {
-		provinceList = coolWeatherDB.loadProvinces();
-		if (provinceList.size() > 0) {
+		provinceList = coolWeatherDB.loadProvinces();//从数据库读取到省级数据
+		if (provinceList.size() > 0) {      //若读取到省级数据，就直接将数据显示到界面上
 			dataList.clear();
 			for (Province province : provinceList) {
 				dataList.add(province.getProvinceName());
@@ -103,7 +119,7 @@ public class ChooseAreaActivity extends Activity {
 			titleText.setText("中国");
 			currentLevel = LEVEL_PROVINCE;
 			} else {
-				queryFromServer(null, "province");
+				queryFromServer(null, "province");//若没有读取到，就调用queryFromServer()方法来从服务器上查询数据
 			}
 	}
 
@@ -151,20 +167,20 @@ public class ChooseAreaActivity extends Activity {
 	*/
 	private void queryFromServer(final String code, final String type) {
 		String address;
+		//根据传入的参数拼装查询地址
 		if (!TextUtils.isEmpty(code)) {
 			address = "http://www.weather.com.cn/data/list3/city" + code + ".xml";
 			} else {
 				address = "http://www.weather.com.cn/data/list3/city.xml";
 			}
 		showProgressDialog();
-		HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+		HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {       //调用HttpUtil的sendHttpRequest()方法来向服务器发送请求，相应的数据回调到onFinish()方法中
 
 			@Override
 			public void onFinish(String response) {
 				boolean result = false;
 				if ("province".equals(type)) {
-					result = Utility.handleProvincesResponse(coolWeatherDB,
-							response);
+					result = Utility.handleProvincesResponse(coolWeatherDB,response);//调用Utility的handleProvincesResponse()方法来解析和处理服务器返回的数据，并存储到数据库中
 					} else if ("city".equals(type)) {
 						result = Utility.handleCitiesResponse(coolWeatherDB,
 								response, selectedProvince.getId());
@@ -173,13 +189,13 @@ public class ChooseAreaActivity extends Activity {
 									response, selectedCity.getId());
 							}
 				if (result) {
-					// 通过runOnUiThread()方法回到主线程处理逻辑
+					// 通过runOnUiThread()方法回到主线程处理逻辑（因为queryProvinces()方法牵扯到UI操作，因此必须在主线程中调用）
 					runOnUiThread(new Runnable() {
 						@Override
 						public void run() {
 							closeProgressDialog();
 							if ("province".equals(type)) {
-								queryProvinces();
+								queryProvinces();//因为数据库已存在了数据，因此调用queryProvinces()就会直接将数据显示到界面上
 								} else if ("city".equals(type)) {
 									queryCities();
 									} else if ("county".equals(type)) {
